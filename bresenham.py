3fcf266b371c6f4472ee3437407a24bafb081f61
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-02-09 18:21:25 -0300

    adicionando a função bresenham

---
+++ bresenham.py
@@ -0,0 +1,41 @@
+
+def bresenham(x0, y0, x1, y1):
+    dx = x1 - x0
+    dy = y1 - y0
+
+    xsign = 1 if dx > 0 else -1
+    ysign = 1 if dy > 0 else -1
+
+    dx = abs(dx)
+    dy = abs(dy)
+
+    if dx > dy:
+        xx, xy, yx, yy = xsign, 0, 0, ysign
+    else:
+        dx, dy = dy, dx
+        xx, xy, yx, yy = 0, ysign, xsign, 0
+
+    D = 2*dy - dx
+    y = 0
+
+    for x in range(dx + 1):
+        yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
+        if D >= 0:
+            y += 1
+            D -= 2*dx
+        D += 2*dy
+
+def matriz_zero(linha,colunas):
+    matrix = []
+    linhas = []
+    for linha in range(linha):
+        for coluna in range(colunas):
+            linhas.append(0)
+        matrix.append(linhas)
+        linhas = []
+    return matrix
+def preencher_matriz_coodenadas(matrix,coodenadas,last_row):
+    for coodenada in coodenadas:
+        matrix[last_row-coodenada[0]][coodenada[1]]=1
+
+
--- visao_geral.py
+++ visao_geral.py
@@ -1,6 +1,5 @@
 from tkinter import Label
-
-
+from bresenham import *
 from tkinter import *
 from tkinter import ttk
 
@@ -18,11 +17,9 @@ class visao_geral:
         for element_linha in self.matrix:
             for element_coluna in element_linha: 
                 if element_coluna:
-                    pixel = Label(self.my_parente,bg="red",bd=4)
-                    print("vermelhor")
+                    pixel = Label(self.my_parente,bg="red",bd=10,relief="raised")
                 else:
-                    pixel = Label(self.my_parente,bg="white",bd=4)
-                    print("azul")
+                    pixel = Label(self.my_parente,bg="white",bd=10,relief="raised")
                 self.labels.append([pixel,(linha,coluna)])
                 coluna+=1
             linha+=1
@@ -35,14 +32,28 @@ class visao_geral:
                 
       
 
-A = [[1,0,0,0,0,0,0],
+'''A = [[1,0,0,0,0,0,0],
      [0,1,0,0,0,0,0],
      [0,0,1,0,0,0,0],
      [0,0,0,1,0,0,0],
      [0,0,0,0,1,0,0],
      [0,0,0,0,0,1,0],
-     [0,0,0,0,0,0,1]]   
+     ]   '''
 root = Tk()
+
+valores = list(bresenham(-1, -4, 3, 2))
+linha,coluna=zip(*valores)
+minimo_coluna = (min(coluna))
+minimo_linha = (min(linha))
+if minimo_coluna < 0 :
+    equalizado = -minimo_coluna
+    new_coluna = list(map(lambda x: x + equalizado,coluna))
+if minimo_linha < 0:
+    equalizado = -minimo_linha
+    new_linha = list(map(lambda x: x + equalizado,linha))
+coordenadas = list(zip(new_linha,new_coluna))
+A= matriz_zero(max(new_linha)+1,max(new_coluna)+1)
+preencher_matriz_coodenadas(A,coordenadas,max(new_linha))
 myapp = visao_geral(root,A)
 root.title("visão geral")
 root.mainloop()
